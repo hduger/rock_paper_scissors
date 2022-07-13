0cb8cfe92af5058c19947cb30c4d5bae36a04df9
Hunter Duger <hduger1@gmail.com>
2022-07-12 22:10:49 -0400

    Added Styling for Winner and Reset Button

--- main.js
+++ main.js
@@ -23,20 +23,6 @@ scissors.addEventListener("click", () => {
     console.log(playerSelection);
     playRound(playerSelection, computerSelection);
 });
-    
-
-// function playerSelect() {
-// // let input = prompt("Choose rock, paper, or scissors", "").toLocaleLowerCase();
-//     let playerSelection;
-
-//     const rock = document.getElementById('#rockButton');
-//     rock.onclick = () => playerSelection = 'rock';
-//     const paper = document.getElementById('#paperButton');
-//     paper.onclick = () => playerSelection = 'paper';
-//     const scissors = document.getElementById('#scissorsButton');
-//     scissors.onclick = () => playerSelection = 'scissors';
-//     return playerSelection
-// }
 
 // This function picks a random number between 1 and 3
 //and it assigns rock, paper, or scissors based on the number
@@ -63,65 +49,42 @@ function playRound() {
         playerSelection==="paper" && computerSelection==="Rock") {
             playerCount++
             plyScore.textContent = playerCount;
-            return "You win!";
+            if (playerCount===5) {
+                declareWinner();
+            }
         } else if (
         (playerSelection==="rock" && computerSelection === "Paper") ||
         (playerSelection==="paper" && computerSelection==="Scissors") ||
         (playerSelection==="scissors" && computerSelection==="Rock")) {
             compCount++;
             compScore.textContent = compCount;
-            return "You Lose!";
+            if (compCount===5) {
+                declareWinner();
+            }
         } else {
             tieCount++;
             tieScore.textContent = tieCount;
             return "Tie!";
         }
 
-
-
-        // if (playerSelection==="paper" && computerSelection==="Scissors") {
-        //     compCount++;
-        //     return "You Lose! Scissors beats paper!";
-        // } else if (playerSelection==="paper" && computerSelection === "Paper") {
-        //     tieCount++;
-        //     return "Tie";
-        // } else if (playerSelection==="paper" && computerSelection==="Rock") {
-        //     playerCount++;
-        //     // plyScore.textContent = `${playerCount}`;
-        //     return "You Win! Paper beats Rock!!";
-        // }
-
-        // if (playerSelection==="scissors" && computerSelection==="Scissors") {
-        //     tieCount++;
-        //     return "Tie!";
-        // } else if (playerSelection==="scissors" && computerSelection === "Paper") {
-        //     playerCount++;
-        //     return "You Win! Scissors beats paper!";
-        // } else if (playerSelection==="scissors" && computerSelection==="Rock") {
-        //     compCount++;
-        //     return "You Lose! Rock beats Scissors!";
-        // }
-    
-
 }
 
-// plyScore.textContent = playerCount.toString();
-// compScore.textContent = compCount.toString();
-// tieScore.textContent = tieCount.toString();
+function declareWinner () {
+    gameResult = document.querySelector("#gameResults");
+    if (playerCount > compCount) {
+        gameResult.textContent= "You Have Beaten The Computer!!!!";
+        gameResult.style.textShadow = "4px 4px 6px green";
+        
+    } else if (compCount > playerCount) {
+        gameResult.textContent= "GAME OVER! The Computer Has Won!";
+        gameResult.style.textShadow = "4px 4px 6px red";
+    }
+}
 
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         playRound();
+const refreshButton = document.querySelector('.refresh-button');
 
-//     }
+const refreshPage = () => {
+  location.reload();
+}
 
-// }
-
-// if (playerCount > compCount) {
-//     console.log("You have beaten the computer!");
-// } else if (compCount > playerCount) {
-//     consol.log("The computer has won!");
-// } else { 
-//     console.log("It is a tie!");
-// }
-// console.log(playerCount, compCount, tieCount);
+refreshButton.addEventListener('click', refreshPage)
